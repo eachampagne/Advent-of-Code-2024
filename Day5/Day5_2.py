34f94adea6309851bb6f7f85e6a142c50312466d
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2024-12-05 15:37:46 -0600

    removed unnecessary line, more commentary

--- Day5/Day5_2.py
+++ Day5/Day5_2.py
@@ -50,7 +50,6 @@ for i in f:
                     ordered_pages.append(page_to_insert)
                     is_sorted = True
                 else:
-                    possibilities = []
                     for j in range(len(ordered_pages)):
                         pages_before = set(ordered_pages[:j])
                         pages_after = set(ordered_pages[j:])
@@ -61,6 +60,8 @@ for i in f:
                 if not is_sorted: #it is possible (at least in theory, though maybe not in my input data) that there isn't yet enough information for there to be a unique place to sort a page. In this case, place it at the end of the "to sort" pile and return later
                     #I coded my algorithm wrong and it actually inserts each page at the first slot that doesn't break any rules, even if that's not the only slot, so this code never runs. It happens that with my input file I get the right final answer anyway (I guess I might have pages in the wrong order, but as long as the middle term is right it doesn't matter). However I can think of edge cases that my code would not work for. I tried, in a different script (not included on GitHub), to address these edge cases. In the process I learned that my input has at least one circular loop of rules (a situation where x|y, y|z, and z|x, requiring that noindividual list of pages contains all of x, y, and z). This means the transitive property doesn't hold, so my plan to create a more general algorithm won't work. I can't prove that there is no way, but I'm accepting that everyone's inputs are designed to be solvable, and I solved the actual problem if not the general case, so I'm not going to put any more time into this.
                     #At any rate, I could have just tried every permutation of pages orders until one worked, so even had I completely abandoned my dubious sorting algorithm I could have solved the problem.
+                    #Update - according to posts online, that would not have taken a very very long time
+                    #Mine works, at least well enough. 
                     pages.insert(page_to_insert)
 
             middle_page_index = int(len(ordered_pages) / 2) #round down to get the target index
