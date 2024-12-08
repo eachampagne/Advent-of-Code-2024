72ef90743c5be939842b334f5902244a0326126f
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2024-12-08 13:43:48 -0600

    move logic check to function for clarity

--- Day8/Day8_1.py
+++ Day8/Day8_1.py
@@ -11,6 +11,9 @@ f = open("input.txt")
 # |
 # v y
 
+def is_in_bounds(antinode, width, height):
+    return (antinode[0] > 0 and antinode[0] <= width and antinode[1] > 0 and antinode[1] <= width)
+           
 antennas = {}
 
 y = 0
@@ -37,9 +40,9 @@ for freq in antennas.keys():
             del_y = antenna_2[1] - antenna_1[1]
             antinode_1 = (antenna_1[0] - del_x, antenna_1[1] - del_y)
             antinode_2 = (antenna_2[0] + del_x, antenna_2[1] + del_y)
-            if antinode_1[0] > 0 and antinode_1[0] <= width and antinode_1[1] > 0 and antinode_1[1] <= width:
+            if is_in_bounds(antinode_1, width, height):
                 antinodes.add(antinode_1)
-            if antinode_2[0] > 0 and antinode_2[0] <= width and antinode_2[1] > 0 and antinode_2[1] <= width:
+            if is_in_bounds(antinode_2, width, height):
                 antinodes.add(antinode_2)
 
 no_antinodes = len(antinodes)
