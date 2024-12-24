effe1afbcc41ae1bc080abba2658d66419fee960
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2024-12-23 21:02:37 -0600

    function is not fast but is much less slow

--- Day23/Day23_2.py
+++ Day23/Day23_2.py
@@ -4,6 +4,8 @@
 #An extraordinarily slow code that does in fact work, it just takes ages
 #I look forward to seeing other people's more efficient ideas
 
+#find_interlinked_set function now passes it's current connections set to the next iteration rather than building from scratch every time. This makes the code run *much* faster. It's still not fast per se, but less slow.
+
 #f = open("sample.txt")
 f = open("input.txt")
 
@@ -14,20 +16,18 @@ largest_set = set()
 
 #recursively builds list of interconnected computers
 #if a group of mutually linked computers are all connected to a single other computer, than the set of the existing set plus the other computer must be a mutually interconnected group as well
-def find_interlinked_set(new_set):
+def find_interlinked_set(new_set, common_connections):
     global size_largest_set
     global largest_set
     if len(new_set) > size_largest_set:
         size_largest_set = len(new_set)
         largest_set = new_set.copy()
-    relevant_connections = [] #a list of all the sets of computers connected to each element in the new set
-    for i in new_set:
-        relevant_connections.append(connections[i])
-    common_connections = set.intersection(*relevant_connections)
     for i in common_connections:
         follow_up_set = new_set.copy()
         follow_up_set.add(i)
-        find_interlinked_set(follow_up_set)
+        existing_connections = common_connections.copy()
+        existing_connections &= connections[i]
+        find_interlinked_set(follow_up_set, existing_connections)
 
 for i in f:
     #parse file
@@ -47,7 +47,9 @@ for i in f:
     common_connections = set_1 & set_2
     for j in common_connections:
         new_triangle = {computer_1, computer_2, j}
-        find_interlinked_set(new_triangle) #if so, check for bigger groupings
+        existing_connections = common_connections.copy()
+        existing_connections &= connections[j]
+        find_interlinked_set(new_triangle, existing_connections) #if so, check for bigger groupings
 
 lan_list = list(largest_set)
 lan_list.sort()
